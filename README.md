0a63c3530e7997207ad0f0ca7a4f6effc593bd20
Kalidou SY <kalidou.harouna.sy@gmail.com>
2021-04-15 22:02:38 +0000

    Flask: Dynamic routes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Introduction to Flask
+Usage of dynamic routes
+
+1. start the flask light server: python app.py
+
+2. Navigate throught this URL: http://127.0.0.1:5000/account/k6SY
+
+3. replace <K6SY> with another work and show the différence
--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 #Importation de la classe Flask du module flask
-from flask import Flask
+from flask import Flask, request
 
 #Instanciation d'un objet de la classe Flask. Cet objet est très utile pour la suite.
 app = Flask(__name__)
 
-
 '''
-Définition d'une view function hello par annotation.
-Format annotation: @app.route(URI,method)
-NB: app represente le nom de l'objet instance de la classe Flask
+Les routes dynamiques sont ajoutés au niveau de l'URI et contiennent des extraits placer entre <>.
+Exemple: /<username>/<token>
+Dans l'exemple ci-dessous, nous avons deux extraits: username et token. Chacun de ces extraits doivent en plus
+être passé comme paramètre à la view fonction.
+
+Ne pas oublier de passer l
 '''
 
-@app.route('/')
-def hello():
-    return "<h1>My first page on flask</h1>"
+@app.route('/account/<username>')
+def account(username):
+    return f'''
+        <h1>My first page on flask</h1>
+        <h2>Welcome {username} to your profile</h2>
+        '''
 
 
-'''
-Le code ci-dessous permet de démarrer l\'application en executant: python app.py
-Assurez vous que l'environnnement virtuel est bien activé
-- app.run() prend en paramètre (facultatif) 
-     - host= ip/fqdn de la machine
-     - port = port sur lequel tourne l\'application
-     - debug = permet d'activer/désactiver le mode debug (hot-reloading garanti)  
-'''
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
