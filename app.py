18189e3072be058bf763347788bde675d662598a
Kalidou SY <kalidou.harouna.sy@gmail.com>
2021-05-07 16:12:49 +0000

    Forms

--- app.py
+++ app.py
@@ -1,13 +1,63 @@
 #Importation de la classe Flask du module flask
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template, session, redirect, url_for
+from custom_forms import *
+
 
 #Instanciation d'un objet de la classe Flask. Cet objet est très utile pour la suite.
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Soroc2022@localhost/emarket'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Soroc2022@localhost/emarket'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'D2FGTHY67UJ_KLIO'
+#db = SQLAlchemy(app)
 
+'''
+@app.route('/categorie', methods=['GET','POST'])
+def manageCategorie():
+    form = Categorie()
+    data=[]
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+        data.append(name)
+        data.append(description)
+        session['data'] = data
+        return redirect(url_for('manageCategorie'))
+
+    return render_template('gestion_stock/categorie/add.html', form=form, items=session.get('data'))
+
+'''
+
+@app.route('/news', methods=['GET','POST'])
+def newletter():
+    myform = Newsletter()
+    name = myform.name.data
+    email = myform.email.data
+    mydate = myform.mydate.data
+    print(f"Test : {name} - {email} - {mydate}")
+    data=[]
+    if myform.validate_on_submit():
+        name = myform.name.data
+        email = myform.email.data
+        mydate = myform.mydate.data
+        data.append(name)
+        data.append(email)
+        data.append(mydate)
+        session['data'] = data
+        print(f"Valide : {name} - {email} - {mydate}")
+        return redirect(url_for('newletter'))
+    return render_template('extra/news.html', form=myform, data=session.get('data'))
+
+    '''
+    data=[]
+    if myform.validate_on_submit():
+        name = myform.name.data
+        email = myform.email.data
+        #mydate = myform.mydate.data
+        data.append(name)
+        data.append(email)
+        session['data'] = data
+        return redirect(url_for('newletter'))
+    '''
 
 
 
@@ -52,24 +102,26 @@ def accueil():
     return render_template('accueil.html')
 
 
-
+'''
 class Produit(db.Model):
     __tablename__ = 'produits'
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(100), unique=True)
     description = db.Column(db.Text, nullable=True)
-
+    categorie_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
 class Categorie(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(100), unique=True)
     description = db.Column(db.Text, nullable=True)
+    produits = db.relationship('Produit', backref='produit',uselist=False)
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -78,10 +130,18 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
 
     def __repr__(self):
         return '<User %r>' % self.username
 
+class ProduitForm():
+    nom = StringField('Indiquer le nom du produit?', validators=[DataRequired()])
+    description = StringField('Indiquer la description du produit?')
+    submit = SubmitField('Submit')
+
+'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=18000, debug=True)
---
+++ custom_forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, DateField, FileField
+from wtforms.validators import DataRequired, Email, InputRequired
+
+class Newsletter(FlaskForm):
+    name = StringField(label='Donnez votre nom?', validators=[DataRequired()]) 
+    email = StringField(label='Merci d\'indiquer votre email?',validators=[InputRequired(), Email(message="Email invalide",check_deliverability=True)])
+    mydate = DateField(label='Indiquer la date de souscription?', validators=[], format='%Y-%m-%d')
+    submit = SubmitField('Submit')
+
+'''
+class Categorie(FlaskForm):
+    name = StringField('Nom', validators=[DataRequired()])
+    description = StringField('Description')
+    submit = SubmitField('Submit')
+'''
